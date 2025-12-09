4d702ea7a165835029bdc9139d506d370908fafc
Yo fp <cosa@correo.com>
2025-12-09 13:41:20 +0100

    control cosa

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ def castLetrCordY(cordXletra,letras):
         if cordXletra==letras[i]:
             return i+1
 
-def dispara(tabla,barcos,cordX,cordY):
-    cords=cordX,cordY
-    for barco in barcos:
-        if cords in barco.getCords:
-            tabla[cordX][cordY]='*'
-        print(barco.getName)
-        print(barco.getCords)
+def disparar(coordenadas):
+
+    if mapajugador1[coordenadas[0]][coordenadas[1]] == "Ç":
+        print("Barco dado ")
+        mapajugador1[coordenadas[0]][coordenadas[1]] = "*"
+    else:
+        print("Agua")
+        mapajugador1[coordenadas[0]][coordenadas[1]] = "X"
 
 def validaX():
     cordXtxt = input("Introduce la cordenada Y del barco (1-10)\n >> ")
@@ -46,10 +47,15 @@ def validaX():
         cordX=int(cordXtxt)
     return cordX
 
-def seleccionaBarco(barcos):
+def seleccionaBarco(barcos,listaBarcos):
     mostrarTabla(barcos)
     print()
     miBarco=input("Introduce el nombre del barco que quieres colocar?\n>> ").lower()
+
+    while miBarco not in listaBarcos:
+        print("Ese barcos no está en el menú de opciones")
+        miBarco = input("Seleccione un barco del menú: ")
+
     for i in range(len(barcos)):
         if barcos[i].getName==miBarco:
             miBarco=barcos[i]
@@ -72,36 +78,55 @@ def seleccionaBarco(barcos):
             barcos.pop(i)
             return miBarco
 
-def ponEnDireccion(tabla, barco, icono):
-    if barco.getOrientation== "derecha":
-        for k in range(barco.getSize):
-            tabla[barco.getCordX][barco.getCordY+ k] = icono
-            barco.agregaCords((barco.getCordX,barco.getCordY+k))
-    if barco.getOrientation== "izquierda":
-        for k in range(barco.getSize):
-            tabla[barco.getCordX][barco.getCordY- k] = icono
-            barco.agregaCords((barco.getCordX, barco.getCordY-k))
 
-    if barco.getOrientation== "abajo":
-        for k in range(barco.getSize):
-            tabla[barco.getCordX + k][barco.getCordY] = icono
-            barco.agregaCords((barco.getCordX+k, barco.getCordY))
+def noPuedesPonerlo(barco):
+    if barco.getCordX==1 and barco.getOrientation=="arriba":
+        return True
 
-    if barco.getOrientation== "arriba":
-        for k in range(barco.getSize):
-            tabla[barco.getCordX- k][barco.getCordY] = icono
-            barco.agregaCords((barco.getCordX-k, barco.getCordY))
+    if barco.getCordY==1 and barco.getOrientation=="izquierda":
+        return True
 
-def ponBarco(barcos,tabla): # Defecto visual al ser izq
-    barco=seleccionaBarco(barcos)
+    if barco.getCordX==10 and barco.getOrientation=="abajo":
+        return True
+
+    if barco.getCordY==10 and barco.getOrientation=="derecha":
+        return True
+
+    return False
+
+
+def ponBarco(barcos,tabla,listaBarcos): # Defecto visual al ser izq
+
+    barco = seleccionaBarco(barcos, listaBarcos)
+
     direccion=input("Hacia que direccion lo quieres poner?\n>> ").lower()
     while direccion not in ["derecha","izquierda","arriba","abajo"]:
         direccion = input("La direccion introducida es incorrecta introduzca una direccion valida: [derecha,izquierda,arriba,abajo]\n >> ").lower()
-
     barco.setOrientation(direccion)
+
+    if noPuedesPonerlo(barco):
+        nueva_direccion = input("No se puede poner un barco en esa direccion, cambia de direccion.\n>> ").lower()
+        while nueva_direccion not in ["derecha", "izquierda", "arriba", "abajo"]:
+            nueva_direccion = input("La direccion introducida es incorrecta introduzca una direccion valida: [derecha,izquierda,arriba,abajo]\n >> ").lower()
+        barco.setOrientation(nueva_direccion)
+
+
     for i in range(len(tabla)):
         for j in range(len(tabla)):
-            ponEnDireccion(tabla,barco,'Ç')
+            if i == barco.getCordX and j == barco.getCordY and barco.getOrientation == "derecha":
+                for k in range(barco.getSize):
+                    tabla[barco.getCordX][barco.getCordY + k] = 'Ç'
+            if i == barco.getCordX and j == barco.getCordY and barco.getOrientation == "izquierda":
+                for k in range(barco.getSize):
+                    tabla[barco.getCordX][barco.getCordY - k] = 'Ç'
+
+            if i == barco.getCordX and j == barco.getCordY and barco.getOrientation == "abajo":
+                for k in range(barco.getSize):
+                    tabla[barco.getCordX + k][barco.getCordY] = 'Ç'
+
+            if i == barco.getCordX and j == barco.getCordY and barco.getOrientation == "arriba":
+                for k in range(barco.getSize):
+                    tabla[barco.getCordX - k][barco.getCordY] = 'Ç'
 
     print()
     imprimirMatrizInvicible(tabla)
@@ -134,65 +159,90 @@ def loHundieron(barco,tabla):
     if contAssrt == barco.getSize:
         barco.hundido
 
-def comienzas(num):
-    ranL=num
-    enviaCosa(ranL)
-    ranEtxt=recibeCosa() #esto sera remplazado por el resultado de la llamada del socket del otro juagdor
-    ranE=int (ranEtxt)
+def addCoordenadas(coordenadas, mapa):
+    if coordenadas in mapa:
+        print("No puedes volver a seleccionar la misma coordenada")
+    else:
+        mapa.append(coordenadas)
 
-    print(ranE)
+    return mapa
 
-    if ranL==ranE:
-        while ranL==ranE:
-            ranL=ran.randint(0,1)
-            enviaCosa(ranL)
-        return comienzas(ranL)
-    elif ranL>ranE:
-        return True
-    elif ranE>ranL:
-        return False
+def pedirCoordenadas():
+    atX = (input("ingresa la coordenada X"))
+    atX = atX.upper()
 
-def enviaCosa(cosa):
+    while not atX in letras:
+        print("Coordeanda x, incorrecta")
+        atX = (input("ingresa la coordenada X"))
+        atX = atX.upper()
+
+    atX = AjustarCoordenadaX(atX)
+    atY = int(input("ingresa la coordenada Y"))
+
+    while atY < 1 or atY > 10:
+        print("Coordenada Y incorrecta")
+        atY = int(input("ingresa la coordenada Y"))
+
+    coordenadas = (atY, atX)
+    return coordenadas
+
+
+def comienzas():
+    miNum = ran.randint(0,1)
+    print("Tu número es:", miNum)
+
+    numEnemigo = int(recibeYEnviaCosa(miNum))
+    print("Rival envió:", numEnemigo)
+
+    if miNum == numEnemigo:
+        print("Empate! repitiendo...")
+        return comienzas()
+
+    return miNum > numEnemigo
+
+
+
+def recibeYEnviaCosa(cosa):
     sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
-    puerto = ("127.0.0.1", 5005)
 
-    casteo=str(cosa).encode()
+    mensaje = str(cosa).encode()
+    sock.sendto(mensaje, ('127.0.0.1', 5005))
 
-    sock.sendto(casteo, puerto)
-    sock.close()
+    info, server = sock.recvfrom(1024)
 
-
-def recibeCosa():
-    sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
-    sock.bind(("127.0.0.1", 5025))
-    print("Esperando por el.")
-
-    info, direccion = sock.recvfrom(1024)
-    print(f"info recibida de: {direccion}")
-
-    dato=info.decode()
+    dato = info.decode()
     sock.close()
 
     return dato
 
+def AjustarCoordenadaX(coordx):
+    coordXs = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    coordResultante = 0
+    for i in range(len(coordXs)):
+        if coordXs[i] == coordx:
+            coordResultante = i + 1
 
+    return coordResultante
 
 letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 numeros = [' 1', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9', '10']
 
-piezas=[]
-barquitos=[]
-portaviones=Barco("portaviones",5)
-acorazado=Barco("acorazado",4)
-destructor=Barco("destructor",4)
-submarino=Barco("submarino",3)
-crucero=Barco("crucero",2)
 
-#barquitos.append(portaviones)
-#barquitos.append(acorazado)
-#barquitos.append(destructor)
+barcosDisponibles = ["portaviones","acorazado","destructor","submarino","lancha"]
+coordenadasMapa = []
+piezas = []
+barquitos = []
+portaviones = Barco("portaviones", 5)
+acorazado = Barco("acorazado", 4)
+destructor = Barco("destructor", 4)
+submarino = Barco("submarino", 3)
+lancha = Barco("lancha", 2)
+
+barquitos.append(portaviones)
+barquitos.append(acorazado)
+barquitos.append(destructor)
 barquitos.append(submarino)
-barquitos.append(crucero)
+barquitos.append(lancha)
 
 for barco in barquitos:
     piezas.append(barco)
@@ -204,27 +254,54 @@ creaMapaVacio(mapajugador2)
 
 imprimirMatrizInvicible(mapajugador1)
 
-print()
+print("--------COLOCAR BARCOS---------")
+while len(piezas) != 0:
+    ponBarco(piezas, mapajugador1,barcosDisponibles)
+
+
 '''
 for i in range(len(piezas)):
     ponBarco(piezas,mapajugador1)'''
 
-
-if comienzas(ran.randint(0,1)): #Esto lo quiero ver con sockets
-    print("Eres el jugador 1\nAhora comienza atacando al jugador 2.")
+barcosHundidos = []
 
 
+
+while len(barcosHundidos) !=len(barcosDisponibles):
+
+    coordenadasJugador = pedirCoordenadas()
+
+    coordenadasMapa = addCoordenadas(coordenadasJugador, coordenadasMapa)
+
+    disparar(coordenadasJugador)
+
+    for barco in barquitos:
+        if barco not in barcosHundidos:
+            hundido = loHundieron(barco, mapajugador1)
+            if hundido is not None:
+                barcosHundidos.append(barco)
+                print(hundido)
+    print()
+    imprimirMatrizInvicible(mapajugador1)
+
+print(">>>>>>>>>>>HAS PERDIDO<<<<<<<<<<")
+print("Fin del programa")
+
+"""salir = True
+while (salir):
+    print("\n-----DISPARAR-------")
+
+    disparox = input("Introduzca una coordenada:  (A-J)")
+    disparoy = int(input("Introduzca una coordenada (1-10)"))
+
+    disparox = ajustarTexto(disparox)
+    disparox = AjustarCoordenadaX(disparox)
+
+    disparoy = AjustarCoordenadaY(disparoy)
+"""
+
+'''
+if comienzas():
+    print("Eres el jugador 1 — comienzas atacando.")
 else:
-    print("Eres el jugador 2, espera a que el jugador uno termine su turno.")
-
-    '''while True:
-
-        atX = int(input("igreseconr X"))
-
-        atY = input("igreseconr Y (A-J)").upper()
-        atYN=castLetrCordY(atY,letras)
-
-
-        dispara(mapajugador1,barquitos, atX, atYN)
-
-        imprimirMatrizInvicible(mapajugador1)'''
+    print("Eres el jugador 2 — espera al jugador 1.")'''
